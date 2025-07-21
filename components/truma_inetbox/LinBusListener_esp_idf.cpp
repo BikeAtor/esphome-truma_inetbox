d76d3419b96dd88f82a5eacd23c4b5bda63ea2cd
BikeAtor <ator@bikeator.com>
2025-07-21 21:24:44 +0200

    Update LinBusListener_esp_idf.cpp

--- components/truma_inetbox/LinBusListener_esp_idf.cpp
+++ components/truma_inetbox/LinBusListener_esp_idf.cpp
@@ -17,7 +17,7 @@ namespace truma_inetbox {
 
 static const char *const TAG = "truma_inetbox.LinBusListener";
 
-#define QUEUE_WAIT_BLOCKING (portTICK_RATE_MS) portMAX_DELAY
+#define QUEUE_WAIT_BLOCKING (portTICK_PERIOD_MS) portMAX_DELAY
 
 void LinBusListener::setup_framework() {
   // uartSetFastReading
